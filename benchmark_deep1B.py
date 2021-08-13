1853f08fae2f806388d55fb42b9a581f8bb9527a
George Williams <george.williams@gmail.com>
2021-08-13 02:46:51 +0000

    new gpu/cpu faiss code and test

--- benchmark_deep1B.py
+++ benchmark_deep1B.py
@@ -71,11 +71,12 @@ ks = [ 10 ] #, 50, 100 ]
 fsearchbs = 8192
 
 # FAISS search parameter set.
-fsearchparams =  [ 32, 34, 36, 40, 50, 60 ] #, 64, 128, 256 ]
-#fsearchparams =  [ 1, 2, 4, 16, 32, 40, 50, 60 ] #, 64, 128, 256 ]
+fsearchparams =  [ 1, 2, 4, 16, 32, 64, 128, 140, 160, 180, 200, 220, 240, 256 ]
+#fsearchparams =  [ 1, 2, 4, 16, 32, 64, 128, 256, 512 ]
 
 # FAISS index file ( set to None if you don't want to benchmark faiss )
-findexfile = "/home/george/Projects/Deep1B_Benchmark_Data/deep-1B.IVF1048576,SQ8.faissindex"
+# 99.32 findexfile = "/home/george/Projects/Deep1B_Benchmark_Data/deep-1B.IVF1048576,SQ8.faissindex"
+findexfile = "/home/george/Projects/BigANN/harsha/big-ann-benchmarks/data/deep-1B.IVF1048576,SQ8.faissindex"
 
 # FAISS search threads
 fsearchthreads = -1 # -1 to use max threads
@@ -100,7 +101,7 @@ def compute_recall(a, b):
     return ninter / a.size, intersect
 
 # Needed by faiss ivf algo.
-def unwind_index_ivf(index):
+def unwind_index_ivfprev(index):
     if isinstance(index, faiss.IndexPreTransform):
         assert index.chain.size() == 1
         vt = index.chain.at(0)
@@ -115,7 +116,7 @@ def unwind_index_ivf(index):
         return None, None
 
 # Needed by faiss ivf algo.
-def rate_limited_iter(l):
+def rate_limited_iterprev(l):
     'a thread pre-processes the next element'
     pool = ThreadPool(1)
     res = None
@@ -137,7 +138,7 @@ def rate_limited_iter(l):
 
 
 # This class wraps the faiss gpu index support.
-class IndexQuantizerOnGPU:
+class IndexQuantizerOnGPUPrev:
 
     def __init__(self, index, search_bs):
         self.search_bs = search_bs
@@ -180,6 +181,188 @@ class IndexQuantizerOnGPU:
         ivf_stats.quantization_time += 1000 * (time.time() - t0)
 
         return D, I
+
+def unwind_index_ivf(index):
+    if isinstance(index, faiss.IndexPreTransform):
+        assert index.chain.size() == 1
+        vt = index.chain.at(0)
+        index_ivf, vt2 = unwind_index_ivf(faiss.downcast_index(index.index))
+        assert vt2 is None
+        return index_ivf, vt
+    if hasattr(faiss, "IndexRefine") and isinstance(index, faiss.IndexRefine):
+        return unwind_index_ivf(faiss.downcast_index(index.base_index))
+    if isinstance(index, faiss.IndexIVF):
+        return index, None
+    else:
+        return None, None
+
+def rate_limited_iter(l):
+    'a thread pre-processes the next element'
+    pool = ThreadPool(1)
+    res = None
+
+    def next_or_None():
+        try:
+            return next(l)
+        except StopIteration:
+            return None
+
+    while True:
+        res_next = pool.apply_async(next_or_None)
+        if res is not None:
+            res = res.get()
+            if res is None:
+                return
+            yield res
+        res = res_next
+
+
+class IndexQuantizerOnGPU:
+    """ run query quantization on GPU """
+
+    def __init__(self, index, search_bs):
+        self.search_bs = search_bs
+        index_ivf, vec_transform = unwind_index_ivf(index)
+        self.index_ivf = index_ivf
+        if vec_transform:
+#            print(type(vec_transform),dir(vec_transform))
+            self.vec_transform = vec_transform.apply
+        else:
+            self.vec_transform = None
+        self.quantizer_gpu = faiss.index_cpu_to_all_gpus(self.index_ivf.quantizer)
+
+
+    def produce_batches(self, x, bs):
+        n = len(x)
+        nprobe = self.index_ivf.nprobe
+        ivf_stats = faiss.cvar.indexIVF_stats
+        for i0 in range(0, n, bs):
+            xblock = x[i0:i0 + bs]
+            t0 = time.time()
+            D, I = self.quantizer_gpu.search(xblock, nprobe)
+            ivf_stats.quantization_time += 1000 * (time.time() - t0)
+            yield i0, xblock, D, I
+
+    def search(self, x, k):
+        bs = self.search_bs
+        if self.vec_transform:
+            x = self.vec_transform(x)
+        nprobe = self.index_ivf.nprobe
+        n, d = x.shape
+        assert self.index_ivf.d == d
+        D = np.empty((n, k), dtype=np.float32)
+        I = np.empty((n, k), dtype=np.int64)
+
+        sp = faiss.swig_ptr
+        stage2 = rate_limited_iter(self.produce_batches(x, bs))
+        t0 = time.time()
+        for i0, xblock, Dc, Ic in stage2:
+            ni = len(xblock)
+            self.index_ivf.search_preassigned(
+                ni, faiss.swig_ptr(xblock),
+                k, sp(Ic), sp(Dc),
+                sp(D[i0:]), sp(I[i0:]),
+                False
+            )
+
+        return D, I
+
+    def range_search(self, x, radius):
+        bs = self.search_bs
+        if self.vec_transform:
+            x = self.vec_transform(x)
+        nprobe = self.index_ivf.nprobe
+        n, d = x.shape
+        assert self.index_ivf.d == d
+
+        sp = faiss.swig_ptr
+        rsp = faiss.rev_swig_ptr
+        stage2 = rate_limited_iter(self.produce_batches(x, bs))
+        t0 = time.time()
+        all_res = []
+        nres = 0
+        for i0, xblock, Dc, Ic in stage2:
+            ni = len(xblock)
+            res = faiss.RangeSearchResult(ni)
+
+            self.index_ivf.range_search_preassigned(
+                ni, faiss.swig_ptr(xblock),
+                radius, sp(Ic), sp(Dc),
+                res
+            )
+            all_res.append((ni, res))
+            lims = rsp(res.lims, ni + 1)
+            nres += lims[-1]
+        nres = int(nres)
+        lims = np.zeros(n + 1, int)
+        I = np.empty(nres, int)
+        D = np.empty(nres, 'float32')
+
+        n0 = 0
+        for ni, res in all_res:
+            lims_i = rsp(res.lims, ni + 1)
+            nd = int(lims_i[-1])
+            Di = rsp(res.distances, nd)
+            Ii = rsp(res.labels, nd)
+            i0 = int(lims[n0])
+            lims[n0: n0 + ni + 1] = lims_i + i0
+            I[i0:i0 + nd] = Ii
+            D[i0:i0 + nd] = Di
+            n0 += ni
+
+        return lims, D, I
+
+
+
+def load_index(idxfile, no_precomputed_tables=True, searchthreads=-1, parallel_mode=3,
+                search_bs=8092 ):
+
+    if not os.path.exists(idxfile):
+        return False
+
+    print("Loading index",idxfile)
+
+    index = faiss.read_index(idxfile)
+
+    index_ivf, vec_transform = unwind_index_ivf(index)
+    if vec_transform is None:
+        vec_transform = lambda x: x
+    if index_ivf is not None:
+        print("imbalance_factor=", index_ivf.invlists.imbalance_factor())
+
+    if no_precomputed_tables:
+        if isinstance(index_ivf, faiss.IndexIVFPQ):
+            print("disabling precomputed table")
+            index_ivf.use_precomputed_table = -1
+            index_ivf.precomputed_table.clear()
+
+    precomputed_table_size = 0
+    if hasattr(index_ivf, 'precomputed_table'):
+        precomputed_table_size = index_ivf.precomputed_table.size() * 4
+    print("precomputed tables size:", precomputed_table_size)
+
+    # prep for the searches
+
+    if searchthreads == -1:
+        print("Search threads:", faiss.omp_get_max_threads())
+    else:
+        print("Setting nb of threads to", searchthreads)
+        faiss.omp_set_num_threads(searchthreads)
+
+    if parallel_mode != -1:
+        print("setting IVF parallel mode to", parallel_mode)
+        index_ivf.parallel_mode
+        index_ivf.parallel_mode = parallel_mode
+
+    ps = faiss.ParameterSpace()
+    ps.initialize(index)
+
+    index_wrap = IndexQuantizerOnGPU(index, search_bs)
+
+    cpu_index = index
+    index = index_wrap
+
+    return [ps, cpu_index, index]
 
 try:
 
@@ -318,32 +501,38 @@ try:
 
         # Reading faiss index file and configure.
         print("Reading faiss index...")
-        index = faiss.read_index(findexfile)
+
+        #GW index = faiss.read_index(findexfile)
+        ret = load_index(findexfile)
+        if ret==False:
+            raise Exception("Cannot read faiss index.")
+        ps, cpu_index, index = ret
         print("Done reading index.")
-        index_ivf, vec_transform = unwind_index_ivf(index)
-        if vec_transform is None:
-            vec_transform = lambda x: x
-        if index_ivf is not None:
-            if DEBUG: print("imbalance_factor=", index_ivf.invlists.imbalance_factor())
-        if DEBUG: print("Index size on disk: ", os.stat(findexfile).st_size)
-        precomputed_table_size = 0
-        if hasattr(index_ivf, 'precomputed_table'):
-            precomputed_table_size = index_ivf.precomputed_table.size() * 4
-        if DEBUG: print("Precomputed tables size:", precomputed_table_size)
 
-        # Configure the search.
-        if fsearchthreads == -1:
-            if DEBUG: print("Search threads:", faiss.omp_get_max_threads())
-        else:
-            if DEBUG: print("Setting nb of threads to", fsearchthreads)
-            faiss.omp_set_num_threads(fsearchthreads)
-        if fparallelmode != -1:
-            if DEBUG: print("Setting IVF parallel mode to", fparallelmode)
-            index_ivf.parallel_mode
-            index_ivf.parallel_mode = fparallelmode
-        ps = faiss.ParameterSpace()
-        ps.initialize(index)
+        if False:
+            index_ivf, vec_transform = unwind_index_ivf(index)
+            if vec_transform is None:
+                vec_transform = lambda x: x
+            if index_ivf is not None:
+                if DEBUG: print("imbalance_factor=", index_ivf.invlists.imbalance_factor())
+            if DEBUG: print("Index size on disk: ", os.stat(findexfile).st_size)
+            precomputed_table_size = 0
+            if hasattr(index_ivf, 'precomputed_table'):
+                precomputed_table_size = index_ivf.precomputed_table.size() * 4
+            if DEBUG: print("Precomputed tables size:", precomputed_table_size)
 
+            # Configure the search.
+            if fsearchthreads == -1:
+                if DEBUG: print("Search threads:", faiss.omp_get_max_threads())
+            else:
+                if DEBUG: print("Setting nb of threads to", fsearchthreads)
+                faiss.omp_set_num_threads(fsearchthreads)
+            if fparallelmode != -1:
+                if DEBUG: print("Setting IVF parallel mode to", fparallelmode)
+                index_ivf.parallel_mode
+                index_ivf.parallel_mode = fparallelmode
+            ps = faiss.ParameterSpace()
+            ps.initialize(index)
 
         # Do the faiss benchmarks.
         for device in [ "gpu", "cpu" ]:
@@ -388,16 +577,22 @@ try:
                     for params in fsearchparams:
                        
                         # Apply param to index
-                        ps.set_index_parameters(index, params)
+                        sparam = "nprobe=%d" % params
+                        #GW ps.set_index_parameters(index, params)
+                        #GW print("fs",type(cpu_index), sparam )
+                        ps.set_index_parameters(cpu_index, sparam)
      
                         # Load index to GPU as needed
                         if device == "gpu":
-                            index_wrap = IndexQuantizerOnGPU(index, fsearchbs)
+                            #GW    index_wrap = IndexQuantizerOnGPU(index, fsearchbs)
+                            index_wrap = index 
+                            #GW print("GPU", type(index_wrap))
                         else:
-                            index_wrap = index
+                            index_wrap = cpu_index
+                            #GW print("CPU", type(index_wrap))
 
                         # Apply param to index
-                        ps.set_index_parameters(index, params) 
+                        #GW ps.set_index_parameters(index, params) 
 
                         # Reset stats
                         ivf_stats = faiss.cvar.indexIVF_stats
@@ -413,7 +608,7 @@ try:
                                 t1 = time.time()
                             else:
                                 t0 = time.time()
-                                D, I = index.search(queries, k)
+                                D, I = cpu_index.search(queries, k)
                                 t1 = time.time()
 
                             diff = t1-t0
